450bacf763512c9148ff90c4ded807239e11a7bb
a-mayoroff <a-mayoroff@ya.ru>
2013-11-23 04:17:24 +0400

    base client functionality for modified protocol supported

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,3 @@
-
 //some globals
 var conn;                       
 var state;    
@@ -7,28 +6,32 @@ const INIT = 0;
 const CONNECTED = 1;
 const LOGIN_WAIT = 2;
 const LOGIN_OK = 3;
+const LOGOUT = 4;
+
+const FETCH_INTERVAL  = 1000;
 
 //entry point                 
 (function () {
-  var $i = $('#i');
 
-  var last_id = 0;
-
+	var fetch_id;
+	var $i = $('#i');
 	state = INIT;
 	
-  conn = new WebSocket("ws://" + window.location.host + "/ws");
-  
+//	conn = new WebSocket("ws://" + window.location.host + "/ws");
+
+	conn = new WebSocket("ws://178.162.76.108:9001/ws");
+	
 	conn.onerror = function () {
 		$('div').hide();
 		document.getElementById('wrapper').style.display = "block";
 		document.getElementById('error').style.display = "block";
-    console.log(arguments);
-  };
+		console.log(arguments);
+		logout();
+	};
 	
 	conn.onconnect = function () {
 		state = CONNECTED;
 	}
-
 
   conn.onmessage = handleMessage; 
 	
@@ -41,15 +44,20 @@ const LOGIN_OK = 3;
 		console.log(e.data);
 		
 		if (STATE == LOGIN_WAIT) {
-				last_id = msg.last_id;
 				STATE = LOGIN_OK;
 				//window.onunload = logout();
+				window.onunload = console.log("unload");
 				document.getElementById('chatwindow').style.display = "block";
 				document.getElementById('login').style.display = "none";
+				fetch_id = start_fetch();
 		} else if (STATE == LOGIN_OK) {
-				console.log(msg.data);
-				last_id = msg.last_id;
-				add_msg(msg.data);
+				//console.log(msg);
+				if ((msg.status == "ok") && (msg.data != undefined)) {
+					var msgs = msg.data;
+					for (var i = 0; i < msgs.length; i++) {
+						add_msg(msgs[i]);
+					}
+				}
 		}
 		
 	}
@@ -59,18 +67,18 @@ const LOGIN_OK = 3;
 		name = $('#name').val();
 		conn.send(JSON.stringify({command: "login", data: name, flags: "", version: "1" }));
 	}
-
-	function fetch() {
-	    conn.send(JSON.stringify({command: "fetch", data: "", flags: "", version: "1" }));
-	}
 	
 	function logout () {
 		console.log("logout");
-		//conn.send(JSON.stringify({command: "logout", data: "", flags: "", version: "1" }));
+		state  = LOGOUT;
+		stop_fetch(fetch_id);	
+		conn.send(JSON.stringify({command: "logout", data: "", flags: "", version: "1" }));
 	}
 
   function add_msg (msg) {
-    $('#history').append('<li>' + msg +'</li>');
+	var date = new Date(msg.time*1000); // multiplied by 1000 so that the argument is in milliseconds, not seconds
+	var formattedTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    $('#history').append('<li>' + formattedTime + " <b>" + msg.author + "</b> " + msg.msg +'</li>');
 	$("#history-wrap").scrollTop($("#history-wrap")[0].scrollHeight);
   }
 
@@ -84,6 +92,17 @@ const LOGIN_OK = 3;
 	return false;
   }
 
+  function start_fetch () {
+	  return setInterval(function () {
+		var msg = {command: "fetch", data: "", flags: "", version: "1"};
+		console.log('fetch', msg);
+		conn.send(JSON.stringify(msg));
+	  }, FETCH_INTERVAL);	
+	}
+
+	function stop_fetch(id) {
+		clearInterval(id);
+	}
   
   $('#send').click(send_to_server);
   $i.focus().keyup(function (e) {
@@ -92,7 +111,6 @@ const LOGIN_OK = 3;
     }
   });
 })();
-
 
 
 function start_robot (name) {
